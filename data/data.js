b98153f00cb2285d1ccdf209869c0cb49f3130db
Bonjourexe <31899191+Bonjourexe@users.noreply.github.com>
2023-08-07 16:02:20 +0200

    restructuration et rédaction

--- data/data.js
+++ data/data.js
@@ -1,5 +1,8 @@
-const courses = {
-    "Accueil": {
+const courses = [
+    {
+        "properties": {
+            "title": "Accueil"
+        },
         "Bonjour !": {
             "content": [
                 "Bienvenue dans FichesDI !",
@@ -10,7 +13,32 @@ const courses = {
                     "content": [
                         "Les fiches sont regroupées par modules.",
                         "Un module constitue un cours. Il a généralement une introduction, un contenu, une conclusion et un glossaire.",
-                        "Mais les fiches peuvent être consultées indépendemment les unes des autres !"
+                        "Mais les fiches peuvent être consultées indépendemment les unes des autres !",
+                        "Les fiches peuvent contenir différents types de blocs :",
+                        {
+                            "type": "note",
+                            "content": [
+                                "Des blocs de notes pour les remarques ou les précisions"
+                            ]
+                        },
+                        {
+                            "type": "warning",
+                            "content": [
+                                "Des blocs d'avertissement pour les choses auxquelles il faut faire attention"
+                            ]
+                        },
+                        {
+                            "type": "example",
+                            "content": [
+                                "Des blocs d'exemple pour illustrer"
+                            ]
+                        },
+                        {
+                            "type": "code",
+                            "content": [
+                                "Des blocs de code pour le code ou le pseudo-code"
+                            ]
+                        }
                     ]
                 },
                 {
@@ -30,17 +58,70 @@ const courses = {
             ]
         }
     },
-    "Programmation": {
+    {
+        "properties": {
+            "title": "Initiation à la programmation"
+        },
         "Introduction": {
-            
+            "content": [
+                "Dans ce module, nous allons voir les bases de la programmation.",
+                "C'est un module assez théorique pour découvrir des concepts fondamentaux de la programmation.",
+                "Le but n'est pas encore de programmer, mais patience, ça arrive ;)",
+                {
+                    "type": "note",
+                    "content": [
+                        "Certaines fiches de ce module comportent des bloc de code. Ils sont reconnaissable à leur fond noir et leur police monospace.",
+                        "Dans ce module, il ne s'agit pas de code à proprement parler, mais de ce qu'on appelle du <<pseudo-code>>.",
+                        "Ça permet de représenter des algorithmes sans pour autant utiliser un vrai langage de programmation. Parfait pour commencer en douceur !"
+                    ]
+                }
+            ]
+        },
+        "Qu'est-ce qu'un algorithme ?": {
+            "content": [
+                "Un algorithme est une suite d'instructions qui permet de résoudre un problème.",
+                "Il prend des entrées, il a des instructions élémentaires simples, et il fourni un résultat.",
+                "Dans notre quotidien, les recettes de cuisine sont des exemples d'algorithmes :",
+                {
+                    "type": "example",
+                    "content": [
+                        {
+                            "type": "code",
+                            "content": [
+                                "Ingrédients et ustensiles :",
+                                "    300 grammes de pâtes",
+                                "    1 litre d'eau",
+                                "    du sel",
+                                "    une casserole",
+                                "    une plaque de cuisson",
+                                "    une passoire",
+                                "",
+                                "Instructions :",
+                                "    mettre l'eau et le sel dans la casserole",
+                                "    mettre la casserole sur la plaque",
+                                "    quand l'eau bout, mettre les pâtes dans la casserole",
+                                "    attendre 10 minutes",
+                                "    verser le contenu de la casserole dans la passoire",
+                                "    servir les pâtes",
+                                "",
+                                "Résultat :",
+                                "    des pâtes cuites."
+                            ]
+                        }
+                    ]
+                },
+                "En programmation c'est la même chose, mais c'est l'ordinateur qui va suivre les instructions du programme.",
+                "Tout ce que fait un ordinateur est décidé par les algorithmes qu'il suit, que ce soit afficher un caractère quand on apuie sur une touche du clavier ou afficher un joli monde en 3D avec des zombies carrés.",
+                "Les algorithmes peuvent être très simple comme très complexes selon le problème à résoudre."
+            ]
         },
         "Les types de données": {
             "content": [
                 "Un programme manipule des données.",
                 "Ces données n'ont pas le même sens selon qu'il s'agisse d'un nombre, d'un texte, de si une case est cochée ou non, etc.",
                 "Il existe donc plusieurs types, qui sont plus ou moins nombreux selon les langages de programmation.",
-                "Le type d'une valeur détermine les opérateurs qu'on peut utiliser dessus et leur fonctionnement.",
-                "En général on retrouve les type :",
+                "Le type d'une valeur détermine les opérateurs (+, -, etc) qu'on peut utiliser dessus et leur fonctionnement.",
+                "En général on retrouve les types :",
                 {
                     "type": "ul",
                     "content": [
@@ -50,7 +131,7 @@ const courses = {
                         "<<string>> pour représenter du texte. Celui-ci est souvent particulier par rapport aux autres"
                     ]
                 },
-                "On peut convertir des valeurs d'un type à l'autre implicitement, çad simplement en utilisant une valeur d'un type au lieu d'un autre type.",
+                "On peut convertir des valeurs d'un type à l'autre implicitement, càd simplement en utilisant une valeur d'un type au lieu d'un autre type.",
                 {
                     "type": "example",
                     "content": [
@@ -138,7 +219,42 @@ const courses = {
             ]
         },
         "Les tableaux": {
-
+            "content": [
+                "Les tableaux sont des objets qui peuvent stocker plusieurs valeurs dans des <<cases>>.",
+                "Un tableau de longueur n comporte n cases.",
+                {
+                    "title": "Les index",
+                    "content": [
+                        "Chaque case est associée à un index (en gros, une adresse) qui permet de l'utiliser, et chacune fonctionne comme une variable.",
+                        "Pour affecter une valeur à une case ou récupérer sa valeur, il faut utiliser le tableau en spécifiant l'index de la case :",
+                        {
+                            "type": "code",
+                            "content": [
+                                "variable mon tableau = nouveau tableau de 10",
+                                "case 0 de mon tableau = 20"
+                            ]
+                        },
+                        "Ici on a créé un tableau d'une longueur de 10 cases puis on a affecté la valeur 20 à sa première case.",
+                        {
+                            "type": "warning",
+                            "content": [
+                                "Dans quasiment tous les langages, l'index (la numérotation des <<cases>>) commence à 0 !",
+                                "Il y a donc un décalage entre le numéro de l'élément et son index :",
+                                {
+                                    "type": "ul",
+                                    "content": [
+                                        "Le premier élément est à l'index 0",
+                                        "Le deuxième est à l'index 1",
+                                        "Le troisième est au 2"
+                                    ]
+                                },
+                                "etc, jusqu'au dernier qui est à l'index n-1 dans un tableau de longueur n.",
+                                "C'est source de confusion et on fait souvent des erreurs bêtes à cause de ça, donc il faut y faire attention."
+                            ]
+                        }
+                    ]
+                }
+            ]
         },
         "Les conditions": {
 
@@ -202,7 +318,10 @@ const courses = {
             
         }
     },
-    "Programmation orientée objet": {
+    {
+        "properties": {
+            "title": "Programmation orientée objet"
+        },
         "Introduction": {
 
         },
@@ -234,7 +353,10 @@ const courses = {
             
         }
     },
-    "HTML": {
+    {
+        "properties": {
+            "title": "HTML"
+        },
         "Introduction": {
             
         },
@@ -254,7 +376,10 @@ const courses = {
             
         }
     },
-    "CSS": {
+    {
+        "properties": {
+            "title": "CSS"
+        },
         "Introduction": {
             
         },
@@ -280,12 +405,48 @@ const courses = {
             
         }
     },
-    "JavaScript": {
+    {
+        "properties": {
+            "title": "JavaScript"
+        },
         "Introduction": {
             
         },
         "Les types de données": {
-
+            "content": [
+                "",
+                {
+                    "title": "Truthy et falsy",
+                    "content": [
+                        "En JavaScript, toutes les valeurs peuvent être converties en booléen.",
+                        "Une valeur est dite truthy si elle devient vrai en booléen, falsy sinon.",
+                        "Les objets sont généralement truthy mais il y a des cas particulier :",
+                        {
+                            "type": "ul",
+                            "content": [
+                                "Un nombre est truthy s'il est différent de 0",
+                                "Un string est truthy s'il n'est pas vide",
+                                "undefined et null sont falsy"
+                            ]
+                        },
+                        {
+                            "type": "warning",
+                            "content": [
+                                "Les tableaux et les objets vides sont des objets et sont donc toujours truthy !",
+                                "Si vous voulez contrôler l'exécution selon qu'un tableau soit vide ou non, il faut utiliser sa longueur :",
+                                {
+                                    "type": "code",
+                                    "content": [
+                                        "if (tableau.length) {",
+                                        "    // vos instructions",
+                                        "}"
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
         },
         "Les opérateurs": {
             "content": [
@@ -324,7 +485,10 @@ const courses = {
             
         }
     },
-    "Quelques API du navigateur": {
+    {
+        "properties": {
+            "title": "Quelques API du navigateur"
+        },
         "Introduction": {
 
         },
@@ -341,7 +505,10 @@ const courses = {
 
         }
     },
-    "Bases de données": {
+    {
+        "properties": {
+            "title": "Bases de données"
+        },
         "Introduction": {
             
         },
@@ -352,7 +519,10 @@ const courses = {
             
         }
     },
-    "Web": {
+    {
+        "properties": {
+            "title": "Web"
+        },
         "Introduction": {
             
         },
@@ -412,7 +582,10 @@ const courses = {
             
         }
     },
-    "Quelques API web": {
+    {
+        "properties": {
+            "title": "Quelques API web"
+        },
         "Introduction": {
             "content": [
                 "Ce module propose simplement quelques exemple d'API web que vous pouvez utiliser dans vos projets.",
@@ -473,7 +646,10 @@ const courses = {
             ]
         }
     },
-    "Git": {
+    {
+        "properties": {
+            "title": "Git"
+        },
         "Introduction": {
 
         },
@@ -484,4 +660,4 @@ const courses = {
 
         }
     }
-}
+]
